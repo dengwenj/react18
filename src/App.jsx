08e6f33587dd7c755556dc4f239b3cacc77048fc
dengwenj <927561237@qq.com>
2022-04-20 21:29:47 +0800

    合成事件

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,17 @@ import UseRoutes from './routes'
 import Demo1 from './demo1'
 
 export default function App() {
+
+  const handleClick = (e) => {
+    console.log(e) // SyntheticBaseEvent 的实例 不是原生的
+    console.log(e.nativeEvent.target)
+  }
+
   return (
     <Router>
       <div>
         <Demo1 />
+        <button onClick={handleClick}>点击</button>
         <NavLink to={'/home'}>home</NavLink>
         <NavLink to={'/about'}>about</NavLink>
 
